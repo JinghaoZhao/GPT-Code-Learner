ecf1679e9bf79dd05940bcf8bb71a3f7f4d7e46c
zhaojinghao <zhaojinghao1996@gmail.com>
2023-05-15 16:08:44 -0700

    Update util with langchain api

--- util.py
+++ util.py
@@ -1,9 +1,9 @@
 import os
-import time
-import openai
 from dotenv import load_dotenv, find_dotenv
 from termcolor import colored
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage
 
 load_dotenv(find_dotenv())
 
@@ -27,33 +27,12 @@ def get_local_llm_response(system_prompt, user_prompt, model="ggml-gpt4all-j", t
     return response
 
 
-def get_openai_response(system_prompt, user_prompt, model="gpt-3.5-turbo",
-                        temperature=0, max_tokens=2048, n=1, patience=100, sleep_time=0):
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+def get_openai_response(system_prompt, user_prompt, model="gpt-3.5-turbo", temperature=0):
+    chat = ChatOpenAI(model_name=model, temperature=temperature)
     messages = [
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": user_prompt},
+        SystemMessage(content=system_prompt),
+        HumanMessage(content=user_prompt)
     ]
-
-    while patience > 0:
-        patience -= 1
-        try:
-            response = openai.ChatCompletion.create(model=model,
-                                                    messages=messages,
-                                                    temperature=temperature,
-                                                    max_tokens=max_tokens,
-                                                    n=n)
-            if n == 1:
-                prediction = response['choices'][0]['message']['content'].strip()
-                if prediction != "" and prediction != None:
-                    return prediction
-            else:
-                prediction = [choice['message']['content'].strip() for choice in response['choices']]
-                if prediction[0] != "" and prediction[0] != None:
-                    return prediction
-
-        except Exception as e:
-            print(e)
-            if sleep_time > 0:
-                time.sleep(sleep_time)
-    return ""
+    response = chat(messages)
+    print(response)
+    return response.content
